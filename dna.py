5cc8df62d137e3c9a4b8cce9c53db2ee00f090b0
Maya Kaul <mk9202@g.rit.edu>
2022-04-11 08:28:49 -0700

    Bro these mans

--- dna.py
+++ dna.py
@@ -1,9 +1,5 @@
-
-
-
-
 from node_types import FrozenNode
-from immutable_list import concatenate
+import immutable_list as il
 
 
 def convert_to_nodes(string):
@@ -11,29 +7,54 @@ def convert_to_nodes(string):
         return
     else:
         value = string[0]
-        next = convert_to_nodes(string[1:])
-        return FrozenNode(value, next)
+        next_1 = convert_to_nodes(string[1:])
+        return FrozenNode(value, next_1)
 
 
-def is_match(first, second):
-    if first is None and second is None:
+def convert_to_string(dna_seq):
+    result = il.to_str(dna_seq.head)
+    return result[1:len(result)-2]
+
+
+def length_rec(dna_seq, count=0):
+    if dna_seq.head is None:
+        return 0
+    else:
+        length_rec(dna_seq.next, count+1)
+        return count
+
+
+
+def is_match(dna_seq1, dna_seq2):
+    if dna_seq1 is None and dna_seq2 is None:
         return True
-    elif first is None or second is None:
+    elif dna_seq1 is None or dna_seq2 is None:
         return False
-    elif first.value is second.value:
-        return is_match(first.next, second.next)
+    elif dna_seq1.value is dna_seq2.value:
+        return is_match(dna_seq1.next, dna_seq2.next)
     else:
         return False
 
 
+def is_pairing(dna_seq1, dna_seq2):
+    if length_rec(dna_seq1) == length_rec(dna_seq2):
+        return True
+    
+
+
+def substitution(dna_seq, idx, base):
+    pass
+
 def insertion(first, second, index):
     if index == 0:
-        return concatenate(first, second)
+        return il.concatenate(first, second)
     elif first is None:
         raise IndexError("Invalid Insertion Index")
     else:
         return FrozenNode(first.value, insertion(first.next, second.next, index-1))
 
+def deletion(dna_seq, idx, segment_size):
+    pass
 
-
-    
+def duplication(dna_seq, idx, segment_size):
+    pass
